13530506dac5b34d682f234ef4169ad1aea1b16a
JoeHyuga <tongtng661@qq.com>
2016-12-11 16:24:13 +0800

    第二节：ASP.NET MVC5+EF6+EasyUI 后台管理系统（2）-EasyUI前端页面框架

---
+++ UnityProject/Web/App_Start/AuthConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Web.App_Start
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        { }
+    }
+}
